67848b67a94584e652fb5cbad481ca715d6df498
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-06-25 16:07:36 +0700

    ep3: player movement + helper

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::{Transform, Window};
+use bevy::input::ButtonInput;
+use bevy::math::Vec3;
+use bevy::prelude::{KeyCode, Res, Transform, Window};
 
 pub struct WindowHelper {}
 
@@ -8,5 +10,33 @@ impl WindowHelper {
         let middle_x = window.width() / 2.0;
         let middle_y = window.height() / 2.0;
         Transform::from_xyz(middle_x, middle_y, 0.0)
+    }
+}
+
+pub struct MovementHelper {}
+
+impl MovementHelper {
+    /// Given a keyboard_input, return the final normalized direction as Vec3
+    pub fn handle_input(keyboard_input: Res<ButtonInput<KeyCode>>) -> Vec3 {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
+            direction += Vec3::new(-1.0, 0.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
+            direction += Vec3::new(1.0, 0.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
+            direction += Vec3::new(0.0, 1.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
+            direction += Vec3::new(0.0, -1.0, 0.0)
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        return direction;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use bevy::DefaultPlugins;
 use bevy::prelude::{
-    App, AssetServer, Camera2dBundle, Commands, Component, default, Query, Res, SpriteBundle,
-    Startup, Window, With,
+    App, AssetServer, ButtonInput, Camera2dBundle, Commands, Component, default, KeyCode, Query,
+    Res, SpriteBundle, Startup, Time, Transform, Update, Window, With,
 };
 use bevy::window::PrimaryWindow;
 
-use bevy_ball::WindowHelper;
+use bevy_ball::{MovementHelper, WindowHelper};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
+        .add_systems(Update, player_movement)
         .run();
 }
 
@@ -40,4 +41,17 @@ fn spawn_player(
         },
         Player {},
     ));
+}
+
+const PLAYER_SPEED: f32 = 500.0;
+
+fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let movement_direction = MovementHelper::handle_input(keyboard_input);
+        transform.translation += movement_direction * PLAYER_SPEED * time.delta_seconds();
+    }
 }
