3fc29b2f96e2fd2a32134f0c096193770e3840c5
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-01 12:58:57 +0700

    ep 6: spawn enemy overtime

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,9 @@ fn main() {
         .init_resource::<StarSpawnTimer>()
         .add_systems(Update, tick_spawn_star_timer)
         .add_systems(Update, spawn_stars_overtime)
+        .init_resource::<EnemySpawnTimer>()
+        .add_systems(Update, tick_spawn_enemy_timer)
+        .add_systems(Update, spawn_enemy_overtime)
         .run();
 }
 
@@ -312,5 +315,48 @@ fn spawn_stars_overtime(
                 },
             ));
         }
+    }
+}
+
+const ENEMY_SPAWN_TIME: f32 = 5.0;
+
+#[derive(Resource)]
+struct EnemySpawnTimer {
+    timer: Timer,
+}
+
+impl Default for EnemySpawnTimer {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+fn tick_spawn_enemy_timer(time: Res<Time>, mut enemy_spawn_timer: ResMut<EnemySpawnTimer>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+fn spawn_enemy_overtime(
+    mut commands: Commands,
+    enemy_spawn_timer: Res<EnemySpawnTimer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    if enemy_spawn_timer.timer.just_finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = RandomHelper::random_f32() * window.width();
+        let random_y = RandomHelper::random_f32() * window.height();
+
+        commands.spawn((
+            Enemy {
+                direction: Enemy::randomize_direction(),
+            },
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+        ));
     }
 }
