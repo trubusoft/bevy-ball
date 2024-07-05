66019de11ea91448304fd9c3ea2c03470892e517
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-05 16:18:43 +0700

    Add different runner on main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,27 @@ use bevy_ball::star::StarPlugin;
 use bevy_ball::system::SystemPlugin;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(SystemPlugin)
-        .add_plugins(PlayerPlugin)
-        .add_plugins(EnemyPlugin)
-        .add_plugins(StarPlugin)
-        .add_plugins(ScorePlugin)
-        .run();
+    Debug::main();
+}
+
+struct Debug;
+
+impl Debug {
+    fn main() {
+        App::new()
+            .add_plugins(DefaultPlugins)
+            .add_plugins(SystemPlugin)
+            .add_plugins(PlayerPlugin)
+            .add_plugins(EnemyPlugin)
+            .add_plugins(StarPlugin)
+            .add_plugins(ScorePlugin)
+            .run();
+    }
+
+    fn debug_x() {
+        App::new()
+            .add_plugins(DefaultPlugins)
+            .add_plugins(SystemPlugin)
+            .run();
+    }
 }
