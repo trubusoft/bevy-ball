84913f0e99e6ea9de197407939231ee87fdf96a5
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-08 13:12:20 +0700

    add run_if in_state

--- src/game/enemy/mod.rs
+++ src/game/enemy/mod.rs
@@ -1,7 +1,9 @@
 use bevy::app::{App, Startup, Update};
-use bevy::prelude::Plugin;
+use bevy::prelude::{in_state, IntoSystemConfigs, Plugin};
 
+use crate::ApplicationState;
 use crate::game::enemy::components::EnemySpawnTimer;
+use crate::game::SimulationState;
 
 pub mod components;
 mod systems;
@@ -12,10 +14,17 @@ impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<EnemySpawnTimer>()
             .add_systems(Startup, systems::spawn_initial_enemies)
-            .add_systems(Update, systems::enemy_movement)
-            .add_systems(Update, systems::confine_enemy_movement)
-            .add_systems(Update, systems::update_enemy_direction_when_out_of_bound)
-            .add_systems(Update, systems::tick_spawn_enemy_overtime)
-            .add_systems(Update, systems::spawn_enemy_overtime);
+            .add_systems(
+                Update,
+                (
+                    systems::enemy_movement,
+                    systems::confine_enemy_movement,
+                    systems::update_enemy_direction_when_out_of_bound,
+                    systems::tick_spawn_enemy_overtime,
+                    systems::spawn_enemy_overtime,
+                )
+                    .run_if(in_state(ApplicationState::InGame))
+                    .run_if(in_state(SimulationState::Running)),
+            );
     }
 }
