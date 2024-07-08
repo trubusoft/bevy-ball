3fbe5d7afaec0cdbc8010706749b59269cdc1c08
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-08 12:55:39 +0700

    add toggle_simulation_state

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,6 +1,7 @@
 use bevy::app::App;
 use bevy::prelude::{in_state, IntoSystemConfigs, Plugin, States, Update};
 
+use crate::ApplicationState;
 use crate::game::enemy::EnemyPlugin;
 use crate::game::player::PlayerPlugin;
 use crate::game::score::ScorePlugin;
@@ -10,6 +11,7 @@ pub mod enemy;
 pub mod player;
 pub mod score;
 pub mod star;
+pub mod systems;
 
 pub struct GamePlugin;
 
@@ -19,7 +21,11 @@ impl Plugin for GamePlugin {
             .add_plugins(PlayerPlugin)
             .add_plugins(EnemyPlugin)
             .add_plugins(StarPlugin)
-            .add_plugins(ScorePlugin);
+            .add_plugins(ScorePlugin)
+            .add_systems(
+                Update,
+                systems::toggle_simulation_state.run_if(in_state(ApplicationState::InGame)),
+            );
     }
 }
 
