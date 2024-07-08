aac62ffde096445b9343c2ec03d606ca80732dd5
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-07-08 13:28:06 +0700

    add OnEnter, OnExi, and run_if in_state for StarPlugin

--- src/game/star/mod.rs
+++ src/game/star/mod.rs
@@ -1,5 +1,8 @@
-use bevy::app::{App, Startup, Update};
-use bevy::prelude::Plugin;
+use bevy::app::{App, Update};
+use bevy::prelude::{in_state, IntoSystemConfigs, OnEnter, OnExit, Plugin};
+
+use crate::ApplicationState;
+use crate::game::SimulationState;
 
 pub mod components;
 mod systems;
@@ -9,8 +12,18 @@ pub struct StarPlugin;
 impl Plugin for StarPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<components::StarSpawnTimer>()
-            .add_systems(Startup, systems::spawn_initial_stars)
-            .add_systems(Update, systems::tick_spawn_stars_overtime)
-            .add_systems(Update, systems::spawn_stars_overtime);
+            .add_systems(
+                OnEnter(ApplicationState::InGame),
+                systems::spawn_initial_stars,
+            )
+            .add_systems(OnExit(ApplicationState::InGame), systems::despawn_all_stars)
+            .add_systems(
+                Update,
+                (
+                    systems::tick_spawn_stars_overtime,
+                    systems::spawn_stars_overtime,
+                )
+                    .run_if(in_state(SimulationState::Running)),
+            );
     }
 }
